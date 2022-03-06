49a9b149c1a827244f2e64aa7ee2bebbefed5f44
Marc Petitdemange <marc.petitdemange57@gmail.com>
2022-03-06 23:04:34 +0100

    dev: Intégration de typescript dans le projet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Header from "./Header";
+
 
 function App() {
   return (
       <h1 className="text-3xl font-bold underline">
         Hello world!
+        <Header/>
       </h1>
-
   );
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,8 @@
+import {Component} from "react";
+
+export default class Header extends Component {
+    render() {
+        return <h1>Bonjour</h1>;
+    }
+}
+
